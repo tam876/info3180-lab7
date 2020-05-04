49c30f217e91f7c43fe0287e76b6a1e3ed61de99
tam876 <tammylee816@yahoo.com>
2020-05-03 23:59:29 -0500

    Completed

--- app/views.py
+++ app/views.py
@@ -6,7 +6,10 @@ This file creates your application.
 """
 
 from app import app
-from flask import render_template, request
+from flask import render_template, request, jsonify
+from app.forms import UploadForm
+from werkzeug.utils import secure_filename
+import os
 
 ###
 # Routing for your application.
@@ -28,6 +31,18 @@ def index(path):
     """
     return render_template('index.html')
 
+@app.route('/api/upload', methods=["POST"])
+def upload():
+    form=UploadForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        description=form.description.data
+        photo = form.photo.data
+        filename=secure_filename(photo.filename)
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+        return jsonify({"message": "File Upload Successful","filename": filename,
+ "description": description})
+    erro=form_errors(form)
+    return jsonify({"errors": erro})
 
 # Here we define a function to collect form errors from Flask-WTF
 # which we can later use
